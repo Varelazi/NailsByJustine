b6d1922cf38cac445b98d02fa537a6a11ad1f029
Ilene Varela-Zul <ilenevarela03@gmail.com>
2024-10-19 02:43:50 -0700

    Added main image and little notice to inform clients to read policies before booking

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,33 @@
-import React, { useEffect } from "react";
-import { ScrollView, View, StyleSheet, StatusBar, Platform, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ScrollView, View, StyleSheet, StatusBar, Platform, Image, Text } from "react-native";
+import * as Font from 'expo-font'; 
 
 export default function Index() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
   useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        'LibreBaskerville': require('../assets/fonts/Libre_Baskerville/LibreBaskerville-Regular.ttf'), 
+      });
+      setFontsLoaded(true); 
+    };
+    
+
+    loadFonts();
     StatusBar.setBarStyle("light-content");
     StatusBar.setBackgroundColor("black");
   }, []);
 
+  if (!fontsLoaded) {
+    return null; 
+  }
+
   return (
-    <ScrollView contentContainerStyle={styles.scrollView} style={styles.container} showsVerticalScrollIndicator={false} >
+    <ScrollView contentContainerStyle={styles.scrollView} style={styles.container} showsVerticalScrollIndicator={false}>
       <View style={styles.content}>
-        <Image source={require('../assets/images/user.png')} style={styles.image} />
+        <Image source={require('../assets/images/BeautyByJustine.png')} style={styles.logo} />
+        <Text style={styles.infoBar}> Please Read Policies Before Booking!</Text>
       </View>
     </ScrollView>
   );
@@ -24,18 +41,25 @@ const styles = StyleSheet.create({
     borderWidth: 3,
   },
   scrollView: {
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0, 
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   content: {
-    flexGrow: 1, 
+    flexGrow: 1,
     alignItems: "center",
     justifyContent: "center",
   },
-  image: {
-    width: 180,
-    height: 180,
-    borderColor: 'yellow',
-    borderWidth: 3,
-    margin: 30
+  logo: {
+    width: 550,
+    height: 190,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  infoBar: {
+    borderColor: 'white',
+    width: '100%',
+    padding: 5,
+    backgroundColor: 'white',
+    textAlign: 'center',
+    fontFamily: 'LibreBaskerville',
   }
 });
